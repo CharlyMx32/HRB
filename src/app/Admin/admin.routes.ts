5389b9b354cc59bb25b6f10fbf791e297602ddd1
CharlyMx32 <centenoavalosc@gmail.com>
2025-02-12 12:42:47 -0600

    Agregar rutas y componentes para la sección de administración

---
+++ src/app/Admin/admin.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component'
+
+export const adminRoutes: Routes = [
+    { path: 'home', component: LayoutComponent },
+];
