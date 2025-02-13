5e56969f1f964215ae9a4b21ca4688c245aa699a
CharlyMx32 <centenoavalosc@gmail.com>
2025-02-13 08:44:32 -0600

    Agregar ruta base y componente de dashboard en la interfaz de administración

--- src/app/Admin/admin.routes.ts
+++ src/app/Admin/admin.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component'
+import { LayoutComponent } from './layout/layout.component';
 import { WerehouseComponent } from './werehouse/werehouse.component';
 import { RegisterComponent } from './register/register.component';
 import { ConfiguracionComponent } from './configuracion/configuracion.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const adminRoutes: Routes = [
-    { path: 'home', component: LayoutComponent },
-    { path: 'almacen', component: WerehouseComponent},
-    { path: 'registro', component: RegisterComponent},
-    { path: 'configuracion', component: ConfiguracionComponent}
+    {
+        path: '', // Ruta base del layout
+        component: LayoutComponent, // Componente principal que contiene la sidebar
+        children: [
+            { path: 'home', component: DashboardComponent },
+            { path: 'almacen', component: WerehouseComponent },
+            { path: 'registro', component: RegisterComponent },
+            { path: 'configuracion', component: ConfiguracionComponent }
+        ]
+    }
 ];
