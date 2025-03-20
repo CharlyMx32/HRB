43f01843805019b6835fc8defd436bb32059b3bf
avinautt12 <avinaluciano50@gmail.com>
2025-03-20 16:17:32 -0600

    cambios de 20/03/2025

--- src/app/Admin/admin.routes.ts
+++ src/app/Admin/admin.routes.ts
@@ -4,6 +4,8 @@ import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { OrdenesComponent } from './ordenes/ordenes.component';
 import { PdfVistaComponent } from '../components/pdf-vista/pdf-vista.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { ProductosComponent } from './productos/productos.component';
 
 export const adminRoutes: Routes = [
     {
@@ -13,8 +15,9 @@ export const adminRoutes: Routes = [
             { path: 'home', component: DashboardComponent },
             { path: 'ordenes', component: OrdenesComponent },
             { path: 'registro', component: RegisterComponent },
+            { path: 'empleados', component: EmpleadosComponent },
             { path: 'facturas', component: PdfVistaComponent },
-            
+            { path: 'productos', component: ProductosComponent }
         ]
     }
 ];
