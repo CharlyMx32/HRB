fc6eaf1dda4b3bbd43d1b6f7288222aa862f3c44
CharlyMx32 <centenoavalosc@gmail.com>
2025-03-25 17:25:23 -0600

    Actualizar URL de API y simplificar el diseño del componente de administración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.252.116:8000/api' // Tu IP para desarrollo
+  apiUrl: 'https://bcb0-187-190-56-49.ngrok-free.app/api' // Tu IP para desarrollo
 };
