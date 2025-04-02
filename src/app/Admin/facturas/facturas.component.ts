42ef05f3ac0b554d7b2f171a567962b701a2bc07
JonathanRios18 <sikra182@gmail.com>
2025-04-01 18:26:55 -0600

    Necesarios

--- src/app/Admin/facturas/facturas.component.ts
+++ src/app/Admin/facturas/facturas.component.ts
@@ -27,11 +27,14 @@ export class FacturasComponent implements OnInit {
     private router: Router
   ) {}
 
+
   ngOnInit(): void {
+    // Obtener facturas desde la API y reemplazar la lista actual
     this.facturasService.getFacturas().subscribe(data => {
       this.facturas = data;
     });
 
+    // Suscribirse al WebSocket sin duplicar facturas
     this.facturasService.facturas$.subscribe(nuevasFacturas => {
       nuevasFacturas.forEach(nuevaFactura => {
         if (!this.facturas.some(factura => factura.URL === nuevaFactura.URL)) {
@@ -40,6 +43,7 @@ export class FacturasComponent implements OnInit {
       });
     });
   }
+
 
   sanitizarURL(url: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
