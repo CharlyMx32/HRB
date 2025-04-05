1fa128318d070f8ef19a4311183d723dbc23dc3a
tovar <jesus.t.morales27@gmail.com>
2025-04-05 15:34:18 -0600

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
 
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrl: 'http://127.0.0.1:8000/api',
+  pusherKey: '06e2ac4b518dea780c81', // Tu key de Pusher
+  pusherCluster: 'us2' // Tu cluster
 
 };
